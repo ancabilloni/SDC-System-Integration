66b14df6e9f9569ba58a1d7d27b3df4fa8de78eb
andrew <hullpod.central@gmail.com>
2017-10-23 18:46:57 +1100

    submission 1

--- ros/build/waypoint_follower/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evotianus/Autonomous_System_Integration/ros/src/waypoint_follower
-# Build directory: /home/evotianus/Autonomous_System_Integration/ros/build/waypoint_follower
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/devel/share/styx_msgs/cmake/styx_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(styx_msgs_MESSAGE_FILES "/home/evotianus/Autonomous_System_Integration/ros/src/styx_msgs/msg/TrafficLight.msg;/home/evotianus/Autonomous_System_Integration/ros/src/styx_msgs/msg/TrafficLightArray.msg;/home/evotianus/Autonomous_System_Integration/ros/src/styx_msgs/msg/Waypoint.msg;/home/evotianus/Autonomous_System_Integration/ros/src/styx_msgs/msg/Lane.msg")
-set(styx_msgs_SERVICE_FILES "")
